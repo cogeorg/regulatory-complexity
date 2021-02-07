7f764a881e711c1f0beb5d2534e1fa3b88f1eccc
Emile Pretorius <emile.dpretorius@gmail.com>
2021-02-07 14:36:05 +0200

    reactivate redirect

--- app/routes.py
+++ app/routes.py
@@ -17,12 +17,12 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
-    # @app.before_request
-    # def before_request():
-    # if not request.is_secure and app.env != "development":
-    #     url = request.url.replace("http://", "https://", 1)
-    #     code = 301
-    #     return redirect(url, code=code)
+@app.before_request
+def before_request():
+if not request.is_secure and app.env != "development":
+    url = request.url.replace("http://", "https://", 1)
+    code = 301
+    return redirect(url, code=code)
 
 @app.route('/')
 @app.route('/index')
