7e54741ba487027f8b4bdd42ce40ab9dd4b9af4c
Emile Pretorius <emile.dpretorius@gmail.com>
2021-07-10 20:24:41 +0200

    update env

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     
     #Comment out below when in heroku environment
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+   # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        #'sqlite:///' + os.path.join(basedir, 'app.db')
 
     #Comment out below when in local environment
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
